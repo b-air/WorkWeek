2ec0c5419989b9a91d90fa155fadb8a9e664fb28
bruno <scarred.eye@gmail.com>
2017-06-15 13:13:32 +0200

    creating fragments structure

---
+++ app/src/main/java/com/example/android/workweek/FragmentPager.java
@@ -0,0 +1,42 @@
+package com.example.android.workweek;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by bruno on 15/06/2017.
+ */
+
+public class FragmentPager extends FragmentPagerAdapter {
+
+    private String tabTitles[] = new String[] { "Numbers", "Family", "Colors", "Phrases" };
+
+    public FragmentPager(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0){
+            return new WorkFragment();
+        } else if (position == 1){
+            return new EatFragment();
+        } else if (position == 2){
+            return new SleepFragment();
+        } else {
+            return new PlayFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return tabTitles[position];
+    }
+}
--- app/src/main/java/com/example/android/workweek/MainActivity.java
+++ app/src/main/java/com/example/android/workweek/MainActivity.java
@@ -3,11 +3,29 @@ package com.example.android.workweek;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.support.design.widget.TabLayout;
+
+import android.support.v4.view.ViewPager;
+
+import static com.example.android.workweek.R.id.viewpager;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(viewpager);
+
+        // Create an adapter that knows which fragment should be shown on each page
+        FragmentPager adapter = new FragmentPager(getSupportFragmentManager());
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapter);
+
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
     }
 }
